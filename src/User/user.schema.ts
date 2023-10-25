edee056dc1cfc1bd9990e8871e69b7343d258e2a
jaisriramr <jaisriram@textmercato.com>
2023-10-25 17:22:55 +0530

    user schema added

---
+++ src/User/user.schema.ts
@@ -0,0 +1,32 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+
+export type UserDocument = User & Document;
+
+@Schema({ timestamps: true })
+export class User {
+  @Prop({ required: true, min: 2 })
+  name: string;
+  @Prop({ required: true })
+  email: string;
+
+  @Prop({ required: true })
+  gender: string;
+
+  @Prop({ required: true })
+  dob: string;
+
+  @Prop({ required: true })
+  profile_picture_url: string;
+
+  @Prop({ required: true })
+  account_type: string; // user, artist
+
+  @Prop({})
+  listeners_count: number;
+
+  @Prop({})
+  verified_account: boolean;
+}
+
+export const UserSchema = SchemaFactory.createForClass(User);
